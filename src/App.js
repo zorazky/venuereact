773ada6d604576ff26a745d0bca2f815701316f7
Oswaldo <oswaldo.vazquez@gmail.com>
2018-11-18 09:25:52 -0600

    venue info section added and animated

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './resources/styles.css';
 import Header from './components/header_footer/Header';
 import Featured from './components/featured/';
+import VenueInfo from './components/venueinfo/';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App" style={{height:"1500px", background:"cornflowerblue"}}>
         <Header />
         <Featured />
+        <VenueInfo />
       </div>
     );
   }
