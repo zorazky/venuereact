db38702de07db537f830f9fd219d752502f42357
Oswaldo <oswaldo.vazquez@gmail.com>
2018-11-18 07:43:38 -0600

    header change from transparent on scroll

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -8,7 +8,25 @@ import SideDrawer from './SideDrawer';
 
 class Header extends Component {
     state = {
-        drawerOpen: false
+        drawerOpen: false,
+        headerShow: false
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll);
+    }
+
+    handleScroll = () => {
+        if(window.scrollY > 0) {
+            this.setState({
+                headerShow: true
+            })
+
+        } else {
+            this.setState({
+                headerShow: false
+            })
+        }
     }
 
     toggleDrawer = (value) => {
@@ -21,7 +39,7 @@ class Header extends Component {
             <AppBar
                 position="fixed"
                 style={{
-                    backgroundColor: '#2f2f2f',
+                    backgroundColor: this.state.headerShow ? '#2f2f2f' : 'transparent',
                     boxShadow: 'none',
                     padding: '10px 0px'
                 }}
